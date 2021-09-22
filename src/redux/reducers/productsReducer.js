9a0ea06e560b3008c771a7303d6b0866b9f8b3f0
AnaRativa09 <anarativa09@gmail.com>
2021-09-21 22:27:09 -0500

    Feat: Reducers adjustQty, addCart, removeCart

--- src/redux/reducers/productsReducer.js
+++ src/redux/reducers/productsReducer.js
@@ -9,6 +9,35 @@ const productsReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case actionTypes.GET_PRODUCTS_LIST:
       return { ...state, dataProducts: action.payload };
+
+    case actionTypes.ADD_TO_CART: {
+      const productData = state.dataProducts.find((product) => product.id === action.payload.id);
+      const inCart = state.cart.find((productInCart) => productInCart.id === action.payload.id);
+
+      return {
+        ...state,
+        cart: inCart
+          ? state.cart.map((productMap) => (productMap.id === action.payload.id
+            ? { ...productMap, qty: productMap.qty + 1 }
+            : productMap))
+          : [...state.cart, { ...productData, qty: 1 }],
+      };
+    }
+
+    case actionTypes.REMOVE_FROM_CART:
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+      };
+
+    case actionTypes.ADJUST_PRODUCT_QTY:
+      return {
+        ...state,
+        cart: state.cart.map((item) => (item.id === action.payload.id
+          ? { ...item, qty: action.payload.qty }
+          : item)),
+      };
+
     default:
       return state;
   }
