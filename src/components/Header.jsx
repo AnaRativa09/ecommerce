58091f35cf6f80135cc4819438ffb905e9f5ef2e
AnaRativa09 <anarativa09@gmail.com>
2021-09-22 11:33:14 -0500

    Feat: Add user photo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ import {
 import Cart from './Cart';
 import '../styles/Header.css';
 import Logo from '../assets/superfuds-logo.jpeg';
+import UserPhoto from '../assets/user-photo.jpeg';
 
 function Header() {
   const [showModal, setshowModal] = useState(false);
@@ -32,69 +33,80 @@ function Header() {
   }, [productsCartState, cartCount]);
 
   return (
-    <Navbar collapseOnSelect expand="lg" className="navbar-bg">
-      <Container>
-        <Link to="/">
-          <img src={Logo} alt="superfuds-logo" className="logo" />
-        </Link>
+    <>
+      <Navbar collapseOnSelect expand="lg" className="navbar-bg">
+        <Container>
+          <Link to="/">
+            <img src={Logo} alt="superfuds-logo" className="logo" />
+          </Link>
 
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Buscar marcas y productos..."
+                className="mr-2"
+                aria-label="Buscar marcas y productos..."
+              />
+            </Form>
 
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Buscar marcas y productos..."
-              className="mr-2"
-              aria-label="Buscar marcas y productos..."
-            />
-          </Form>
+            <Button
+              className="custom-btn-without-bg white-font flex-row"
+              onClick={() => setshowModal(true)}
+            >
+              <i className="fas fa-shopping-cart" />
+              <p className="p-without-margin">{cartCount}</p>
+            </Button>
 
-          <Button
-            className="custom-btn-without-bg white-font flex-row"
-            onClick={() => setshowModal(true)}
-          >
-            <i className="fas fa-shopping-cart" />
-            <p className="p-without-margin">{cartCount}</p>
-          </Button>
+            <div className="user-container">
+              <div className="user-description">
+                <p className="bold-font white-font p-without-margin">Saiby Alimentos</p>
+                <p className="thin-font p-without-margin green-dark-font">
+                  Mi perfil
+                  <i className="fas fa-chevron-down" />
+                </p>
+              </div>
+              <img className="user-profile-photo" src={UserPhoto} alt="user-profile" />
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-          <Modal show={showModal} size="lg" onHide={() => setshowModal(false)}>
-            <Modal.Header>
-              <Modal.Title className="text-description">
-                <Button
-                  className="custom-btn-without-bg green-font"
-                  type="button"
-                  onClick={() => setshowModal(false)}
-                >
-                  <i className="fas fa-chevron-left green-font" />
-                </Button>
-                <p className="p-without-margin"> Volver a la tienda </p>
-              </Modal.Title>
-            </Modal.Header>
+      <Modal show={showModal} size="lg" onHide={() => setshowModal(false)}>
+        <Modal.Header>
+          <Modal.Title className="text-description">
+            <Button
+              className="custom-btn-without-bg green-font"
+              type="button"
+              onClick={() => setshowModal(false)}
+            >
+              <i className="fas fa-chevron-left green-font" />
+            </Button>
+            <p className="p-without-margin"> Volver a la tienda </p>
+          </Modal.Title>
+        </Modal.Header>
 
-            <Modal.Body>
-              {
-                productsInCart.length === 0
-                  ? (
-                    <>
-                      <p>No tienes productos en el carrito, aún...</p>
-                      <Button
-                        variant="primary"
-                        className="custom-btn"
-                        onClick={() => setshowModal(false)}
-                      >
-                        Comprar ahora
-                      </Button>
-                    </>
-                  )
-                  : <Cart />
-              }
-
-            </Modal.Body>
-          </Modal>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Modal.Body>
+          {
+            productsInCart.length === 0
+              ? (
+                <>
+                  <p>No tienes productos en el carrito, aún...</p>
+                  <Button
+                    variant="primary"
+                    className="custom-btn"
+                    onClick={() => setshowModal(false)}
+                  >
+                    Comprar ahora
+                  </Button>
+                </>
+              )
+              : <Cart />
+          }
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
 
