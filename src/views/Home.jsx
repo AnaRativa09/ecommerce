a67ca5504a5c930082d741b0be9e799ae27cf79d
AnaRativa09 <anarativa09@gmail.com>
2021-09-22 08:46:35 -0500

    Feat: Add slider to show products

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import { getProducts } from '../redux/actions/productsActions';
 
@@ -16,22 +19,36 @@ function Home() {
     getAllProducts();
   }, []);
 
+  const settingsSlider = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+  };
+
   return (
-    <>
-      <h1>
+    <section className="container mt-5 carousel">
+      <h2 className="slider-title">
         Nuevo en Superfüds
         <span className="green-font"> Ver más</span>
-      </h1>
-      <section className="products-slider">
+      </h2>
+
+      <Slider {...settingsSlider}>
         {
           products !== []
             ? products.map((product) => (
-              <ProductCard key={product.id} dataProduct={product} />
+              <div className="card-wrapper" key={product.id}>
+                <div className="card">
+                  <ProductCard dataProduct={product} />
+                </div>
+              </div>
             ))
             : null
         }
-      </section>
-    </>
+      </Slider>
+    </section>
   );
 }
 
