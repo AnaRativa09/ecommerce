19e744d6d556f453dac764222e5f3ca572c40cb1
AnaRativa09 <anarativa09@gmail.com>
2021-09-22 14:05:18 -0500

    Config: Add linebreak-style to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     'react/prop-types': 'off',
     'react/destructuring-assignment': 'off',
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
-    'react/jsx-props-no-spreading': 'off'
+    'react/jsx-props-no-spreading': 'off',
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix']
   },
 };
