70cc5cb42f48acb936e6fa96fbe37bed7ee31e6c
AnaRativa09 <anarativa09@gmail.com>
2021-09-21 23:02:57 -0500

    Feat: add total price cart

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import ProductCartItem from '../components/ProductCartItem';
+import getFormatPrice from '../functions';
 import '../styles/Cart.css';
 
 function Cart() {
   const productsCartState = useSelector((state) => state.products.cart);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    let price = 0;
+    productsCartState.forEach((item) => {
+      price += item.qty * item.price_real;
+    });
+
+    setTotalPrice(getFormatPrice(price));
+  }, [productsCartState, totalPrice, setTotalPrice]);
 
   return (
     <section className="cart-section">
@@ -33,6 +44,7 @@ function Cart() {
             : null
         }
       </table>
+      <h3>{`Total: $${totalPrice}`}</h3>
     </section>
   );
 }
