a467c3e4db749bc5d0ec8e3999798e462a26b8cd
AnaRativa09 <anarativa09@gmail.com>
2021-09-22 10:34:59 -0500

    Refactor: Add num parameter to getShortTitle func

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,5 @@
 export const getFormatPrice = (price) => price.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');
 
-export const getShortTitle = (title) => (title.length > 15 ? `${title.substring(0, 15)}...` : title);
+export const getShortTitle = (title, num) => (title.length > num ? `${title.substring(0, num)}...` : title);
 
 export const getTotalPriceQty = (qty, priceProduct) => `${parseFloat(priceProduct) * qty}`;
