ea19b0cd180300ca38d1f46f9942ed99afd2a3fa
AnaRativa09 <anarativa09@gmail.com>
2021-09-21 20:41:59 -0500

    Feat: Add router and components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Import components
-import Home from './components/Home';
+import Header from './components/Header';
+import Home from './views/Home';
+import Cart from './views/Cart';
+import NotFound from './views/NotFound';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/cart" component={Cart} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
